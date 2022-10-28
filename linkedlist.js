bde18b5dffeef4b473932ed605d7b0f164f97bc1
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-10-28 18:26:45 -0500

    finished excercise

--- linkedlist.js
+++ linkedlist.js
@@ -1,1 +1,237 @@
 
+class Node {
+
+    constructor(data = null){
+        this.data = data;
+        this.next = null;
+
+    }
+}
+
+class LinkedList {
+
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+        console.log(`Original:\n ${this.printList()}`);
+    }
+
+    fillValues(arr){
+        // if list is Empty 
+        if (arr.length === 0){
+            this.tail = this.head;
+        } 
+        else if(arr.length > 0){
+            this.head = new Node(arr[0]);
+            let current = this.head;
+            for (let i = 1; i < arr.length; i++) {
+                current.next = new Node(arr[i]); // create a new node
+                current = current.next;
+            }
+            this.tail = current;
+            this.size = (arr.length);
+        }
+        console.log(`Filled list with values ${arr} :\n ${this.printList()}`);
+        return this;
+    }
+
+    printList() {
+        let currNode = this.head;
+        let listStr = "";
+    
+        if (currNode) {
+            listStr = `${currNode.data}`;
+    
+            while (currNode.next) {
+                currNode = currNode.next;
+                listStr = `${listStr} --> ` + `${currNode.data}`;
+            }
+        }
+        return listStr + ` --> ${null}`;
+    }
+
+    toArray() {
+        let currNode = this.head;
+        const listArr = [];
+
+        if (currNode) {
+            listArr.push(currNode.data);
+
+            while (currNode.next) {
+                currNode = currNode.next;
+                listArr.push(currNode.data);
+            }
+        }
+        return listArr;
+    }
+
+    addToTail(data){
+        let newNode = new Node(data); // create a new node
+        let currNode; // store current node
+    
+        // if list is Empty add the data element and make it head
+        if (this.head === null){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+        // if list has some data, take the head as the current node
+            currNode = this.head;
+    
+            // iterate over each node till we reach the last one at the end of the list
+            // we enter the while only when a next node exists
+            while (currNode.next) {
+                currNode = currNode.next;
+            }
+            // add node next to the last and current one
+            currNode.next = newNode;
+            this.tail = currNode.next;
+        }
+        this.size++;  // increment the list size by 1
+        console.log(`Added ${data} to Tail:\n ${this.printList()}`);
+
+        return this.tail;
+    }
+
+    addToHead(data){
+        let newHead = new Node(data); // create a new node that will be our new head node
+
+        if (this.head === null){
+            this.head = newHead;
+            this.tail = this.head;
+        } else{
+            newHead.next = this.head; // assign the next to the current head
+            this.head = newHead; // and reassign the head to our new head node
+        }
+        // increment the list size by 1
+        this.size++;
+        console.log(`Added ${data} to Head:\n ${this.printList()}`);
+
+        return this.head;
+    }
+
+    addToMid(data){
+        const newNode = new Node(data);
+
+        // if list is Empty 
+        if (this.head === null){
+            this.head = newNode;
+            this.tail = this.head;
+        } 
+        else {
+            let prevMidNode = this.head;
+            let nextMidNode = this.head.next;
+        
+            for (let i = 0; i < this.size / 2 - 1; i++) {
+                prevMidNode = prevMidNode.next;
+            }
+    
+            nextMidNode = prevMidNode.next;
+            prevMidNode.next = newNode;
+            newNode.next = nextMidNode;
+        }
+        // increment the list size by 1
+        this.size++;
+        console.log(`Added ${data} to Middle:\n ${this.printList()}`);
+        return this;
+    }
+
+    removeHead(){
+        // if list is Empty 
+        if (this.head === null){
+            this.tail = this.head;
+        }
+        // only remove head if the list is not empty
+        else if (this.size > 0){
+            this.head = this.head.next;
+            this.size--;
+        }
+        console.log(`Removed Head:\n ${this.printList()}`);
+        return this.head;
+    }
+
+    removeTail(){
+        let newTail = this.head;
+
+        // if list is Empty 
+        if (this.head === null){
+            this.tail = this.head;
+        } 
+        // only remove tail if the list is not empty
+         if (this.size > 0){
+            for (let i = 0; i < this.size - 2; i++) {
+                newTail = newTail.next;
+            }
+            newTail.next = null;
+            this.tail = newTail;
+        }
+        console.log(`Removed Tail:\n ${this.printList()}`);
+        return this.tail;
+    }
+    
+    removeMid() {
+        // if list is Empty 
+        if (this.head === null){
+            this.tail = this.head;
+        } 
+        // only remove tail if the list is not empty
+        if (this.size > 0) {
+          let midNode = this.head;
+    
+          if (this.size % 2 === 0) {
+            for (let i = 0; i < this.size / 2 - 1; i++) {
+              midNode = midNode.next;
+            }
+          } else {
+            for (let i = 0; i < this.size / 2 - 2; i++) {
+              midNode = midNode.next;
+            }
+          }
+          midNode.next = midNode.next.next;
+          // decrease list size by 1
+          this.size--;
+          console.log(`Removed value from the middle:\n ${this.printList()}`);
+
+        }
+        return this;
+}
+    
+    reverseList(){
+        let reversedArr;
+
+        // if list is Empty 
+        if (this.head === null){
+            this.tail = this.head;
+        } 
+        // only if the list is not empty
+        else if (this.size > 0){
+            // convert list to array and reverse it
+            reversedArr = this.toArray().reverse();
+        }
+
+        // using a previously declared method to fill in the values
+        return this.fillValues(reversedArr);
+    }
+
+    sortList(){
+        let sortedArr;
+
+        // if list is Empty 
+        if (this.head === null){
+            this.tail = this.head;
+        } 
+        // only if the list is not empty
+        else if (this.size > 0){
+            // convert list to array and reverse it
+            sortedArr = this.toArray().sort((a, b) => b - a);
+        }
+
+        // using a previously declared method to fill in the values
+        return this.fillValues(sortedArr);
+    }
+
+}
+
+
+module.exports = LinkedList;
--- linkedlist.test.js
+++ linkedlist.test.js
@@ -1,1 +1,117 @@
+//The tests we are going to use for this are unit tests (the unit is a Class)
 
+// Using ES instead of Common JS to import
+// import LinkedList from "./linkedlist.js";
+
+const LinkedList = require("./linkedlist");
+
+//   |node|
+//   Example:    |head|  -->  |head.next|  -->  |head.next.next|  -->  |tail(head.next.next.next)|  -->  null
+describe("Testing the linked list class in 8 cases", () => {
+
+    it("Add a new value to the list's tail", () => {
+        const list = new LinkedList;
+
+        expect((list.addToTail)).toBeDefined()
+        expect((list.head && list.tail && list.size)).toBeFalsy();
+        expect((list.addToTail(2)).data).toBe(2);
+        expect((list.head === list.tail)).toBeTruthy()
+        expect((list.head && list.tail && list.size)).not.toBeNull();
+        expect((list.addToTail(4)).data).toBe(4);
+        expect((list.addToTail("six")).data).toBe("six");
+        expect((list.addToTail(false)).data).toBe(false);
+        expect((list.size)).toBe(4);
+        expect((list.head.data)).toBe(2);
+        expect((list.tail.data)).toBe(false);
+    })
+    
+    it("Add a new value to the list's head", () => {
+        const list = new LinkedList;
+
+        expect(list.addToHead).toBeDefined();
+        expect((list.head && list.tail && list.size)).toBeFalsy();
+        expect((list.addToHead(2)).data).toBe(2);
+        expect((list.head === list.tail)).toBeTruthy()
+        expect((list.head && list.tail && list.size)).not.toBeNull();
+        expect((list.addToHead(4)).data).toBe(4);
+        expect((list.addToHead("six")).data).toBe("six");
+        expect((list.addToHead(false)).data).toBe(false);
+        expect((list.size)).toBe(4);
+        expect((list.head.data)).toBe(false);
+        expect((list.tail.data)).toBe(2);
+    })
+
+    it("Add a new value to the list's middle", () => {
+        const list = new LinkedList;
+        
+        expect(list.addToMid).toBeDefined();
+        expect((list.head && list.tail && list.size)).toBeFalsy();
+        
+        list.fillValues([1,3]);
+        expect((list.head && list.tail && list.size)).toBeTruthy();
+        expect(list.addToMid(2).head.next.data).toBe(2);
+        expect(list.size === list.addToMid().size).toBe(false);
+    })
+
+    it("remove the head of the list", () => {
+        const list = new LinkedList;
+        
+        expect(list.removeHead).toBeDefined();
+        expect(list.removeHead()).toBe(null);
+
+        list.fillValues([false,1,2,"three",4,5]);
+        expect((list.head && list.tail && list.size)).toBeTruthy();
+        expect((list.head.data)).toBe(false);
+        expect(list.removeHead().data).toBe(1);
+    })
+
+    it("Remove the tail of the list", () => {
+        const list = new LinkedList;
+        
+        expect(list.removeTail).toBeDefined();
+        expect(list.removeTail()).toBe(null);
+
+        list.fillValues([false,1,2,"three",4,5]);
+        expect((list.head && list.tail && list.size)).toBeTruthy();
+        expect((list.tail.data)).toBe(5);
+        expect(list.removeTail().data).toBe(4);
+    })
+
+    it("Remove the middle of the list", () => {
+        const list = new LinkedList;
+        
+        expect(list.removeMid).toBeDefined();
+        expect(list.removeMid().tail).toBe(null);
+        
+        list.fillValues([false,1,2,"three",4,5]);
+        expect((list.head && list.tail && list.size)).toBeTruthy();
+        expect(list.size === list.removeMid().size).toBe(false);
+    })
+    
+    it("Reverse the list", () => {
+        const list = new LinkedList;
+        
+        expect(list.reverseList).toBeDefined();
+        expect((list.head && list.tail && list.size)).toBeFalsy();
+
+        list.fillValues([false,1,2,"three",4,5]);
+        expect((list.head && list.tail && list.size)).toBeTruthy();
+        expect(list.reverseList().size === list.size).toBe(true);
+        expect(list.tail.data === list.reverseList().head.data).toBe(true);
+        expect(list.reverseList().head.data).toBe(5);
+    })
+
+    it("Add a new value at the middle of the list", () => {
+        const list = new LinkedList;
+        
+        expect(list.sortList).toBeDefined();
+        expect((list.head && list.tail && list.size)).toBeFalsy();
+
+        list.fillValues([8,1,2,11,0,4,5,22]);
+        expect((list.head && list.tail && list.size)).toBeTruthy();
+        expect(list.sortList().size === list.size).toBe(true);
+        expect(list.sortList().head.data).toBe(22);
+        expect(list.sortList().tail.data).toBe(0);
+    })
+
+})
